0a3f9437417cbbe26089d79aa3a0b92bc16df21b
Natanael Ricardo <nathanshurle@gmail.com>
2022-06-15 08:54:36 -0300

    add classes comment e post

---
+++ src/entities/Post.java
@@ -0,0 +1,70 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	private Date moment;
+	private String title;
+	private String content;
+	private String likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	public Post() {
+		
+	}
+
+	public Post(Date moment, String title, String content, String likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getLikes() {
+		return likes;
+	}
+
+	public void setLikes(String likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+	
+	public void addComments(Comment comment) {
+		comments.add(comment);
+	}
+	
+	public void removeComments(Comment comment) {
+		comments.remove(comment);
+	}
+	
+}
